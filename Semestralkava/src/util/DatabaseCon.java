71c89eeeb883b8dcc0b567f8fc5f2a272443dab2
Jindreak <36510862+Jindreak@users.noreply.github.com>
2018-06-02 20:07:28 +0200

    preliti seznamu z databaze

--- Semestralkava/src/util/DatabaseCon.java
+++ Semestralkava/src/util/DatabaseCon.java
@@ -12,11 +12,110 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 
 /**
  *
  * @author Jindra
  */
 public class DatabaseCon {
+    
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:src\\semestralkava\\db\\kava.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    
+    public SeznamKav nactiKavy(){
+        String sql =    "SELECT id, Nazev, Popis, Zeme_puvodu FROM Kava" ;
+        SeznamKav seznamKav = new SeznamKav();            
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            
+            while (rs.next()) {
+                
+                Kava kava = new Kava(rs.getInt("id"),
+                                     rs.getString("Nazev"),
+                                     rs.getString("Popis"),
+                                     rs.getString("Zeme_puvodu"));
+                
+                seznamKav.pridejKavu(kava);
+                
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return seznamKav;
+    }
+    
+    public SeznamKavaren nactiKavarny(){
+        String sql =    "SELECT id, Nazev, Adresa FROM Kavarna" ;
+        SeznamKavaren seznamKavaren = new SeznamKavaren();            
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            
+            while (rs.next()) {
+                
+                String input = rs.getString("Adresa");
+                String[] elements = input.split(" ");
+                
+                
+                Kavarna kavarna = new Kavarna(  rs.getString("Nazev"),
+                                                rs.getInt("id"),
+                                                elements[0],
+                                                Integer.parseInt(elements[1]),
+                                                elements[2],
+                                                Integer.parseInt(elements[3]));
+                
+                seznamKavaren.pridejKavarnu(kavarna);
+                
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return seznamKavaren;
+    }
+    
+    public void vypisHodnoceni(){
+        String sql =    "SELECT Nazev, Popis, Zeme_puvodu, Pocet, Datum\n" +
+                        "FROM Hodnoceni_kavy\n" +
+                        "INNER JOIN Hodnoceni ON hodnoceni_id = Hodnoceni.id\n" +
+                        "INNER JOIN Kava ON kava_id = Kava.id;";
+                    
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" + 
+                                   rs.getString("Nazev") + "\t" +
+                                   rs.getString("Popis") + "\t" +
+                                   rs.getString("Zeme_puvodu")+ "\t");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public DatabaseCon (){
+        
+    }
+    
+    
     
 }
